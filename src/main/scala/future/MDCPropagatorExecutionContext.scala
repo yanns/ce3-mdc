9ca7fc965768e6ec184fd945efe9028d3c24c09f
Yann Simon <yann.simon@commercetools.com>
2021-12-21 10:12:38 +0100

    simplify MDCPropagatorExecutionContext

--- src/main/scala/future/MDCPropagatorExecutionContext.scala
+++ src/main/scala/future/MDCPropagatorExecutionContext.scala
@@ -13,35 +13,21 @@ trait MDCPropagatorExecutionContext extends ExecutionContextExecutor {
 
   override def prepare(): ExecutionContext = new ExecutionContext {
     // capture the MDC
-    val context = getCopyOfContext()
+    val context = MDC.getCopyOfContextMap()
 
-    def execute(r: Runnable) = self.execute(propagatingRunnable(context, r))
-    def reportFailure(t: Throwable) = self.reportFailure(t)
-  }
+    def execute(r: Runnable): Unit =
+      self.execute(() => {
+        // Run the runnable with the captured context
+        setContext(context)
+        r.run()
+      })
 
-  private def getCopyOfContext(): Context = MDC.getCopyOfContextMap
-
-  private def propagatingRunnable(context: Context, r: Runnable): Runnable =
-    () => withContext(context)(r.run())
-
-  private def withContext[R](context: Context)(fn: => R): Unit = {
-    // backup the callee context
-    val oldMDCContext = getCopyOfContext()
-
-    // Run the runnable with the captured context
-    setContext(context)
-    try fn
-    finally
-    // restore the callee MDC context
-    setContext(oldMDCContext)
+    def reportFailure(t: Throwable): Unit = self.reportFailure(t)
   }
 
   private def setContext(context: Context): Unit =
-    if (context == null) {
-      MDC.clear()
-    } else {
-      MDC.setContextMap(context)
-    }
+    if (context == null) MDC.clear()
+    else MDC.setContextMap(context)
 }
 
 object MDCPropagatorExecutionContext {
